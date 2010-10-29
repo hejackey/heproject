20976e747b39b4179317dbf61a5922ff6ee6b52d
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2010-10-29 00:27:26 +0000

    最新版本

--- trunk/alexaParser/src/com/jackey/security/DESDemo.java
+++ trunk/alexaParser/src/com/jackey/security/DESDemo.java
@@ -5,8 +5,6 @@ import java.security.SecureRandom;
 import javax.crypto.Cipher;   
 import javax.crypto.KeyGenerator;   
 import javax.crypto.SecretKey;   
-import javax.crypto.SecretKeyFactory;   
-import javax.crypto.spec.DESKeySpec;   
 import javax.crypto.spec.SecretKeySpec;
   
   
