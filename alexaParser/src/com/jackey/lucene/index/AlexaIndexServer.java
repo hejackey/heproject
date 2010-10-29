e5aa447a07cc60d3fc48c1cf8bba95e04f0d16b5
xiaohu0901 <xiaohu0901@hotmail.com>
2010-10-29 02:34:01 +0000

    最新版本

--- alexaParser/src/com/jackey/lucene/index/AlexaIndexServer.java
+++ alexaParser/src/com/jackey/lucene/index/AlexaIndexServer.java
@@ -2,6 +2,7 @@ package com.jackey.lucene.index;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -9,6 +10,11 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 
 import com.jackey.lucene.dao.AlexaDao;
 import com.jackey.parser.bean.AlexBean;
@@ -37,15 +43,56 @@ public class AlexaIndexServer extends BaseIndexServer {
 						
 			writer.close();
 		}catch(CorruptIndexException e){;
-			log.error("create AlexaIndexServer CorruptIndexException:"+e.getMessage());
+			log.error("create createIndex CorruptIndexException:"+e.getMessage());
 		}
 		catch(IOException ex){;
-			log.error("create AlexaIndexServer IOException:"+ex.getMessage());
+			log.error("create createIndex IOException:"+ex.getMessage());
 		}
 		catch (SQLException e) {
-			log.error("create AlexaIndexServer SQLException:"+e.getMessage());
+			log.error("create createIndex SQLException:"+e.getMessage());
 			e.printStackTrace();
 		}
 	}
 
+	public List searchIndex(String column,String key){		
+		try {
+			IndexSearcher isearcher = getIndexSearch();
+			QueryParser parser = getQueryParser(column);			
+			Query query = parser.parse(key);
+			
+			ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+			List alexList = new ArrayList();
+			
+			for (int i = 0; i < hits.length; i++) {
+				Document hitDoc = isearcher.doc(hits[i].doc);
+				System.out.println("title="+hitDoc.get("title")+","+
+						"domain="+hitDoc.get("domain")+","+
+						"desc="+hitDoc.get("desc")+","+
+						"sorttype="+hitDoc.get("sorttype"));
+				
+				AlexBean bean = new AlexBean();
+				bean.setTitle(hitDoc.get("title"));
+				bean.setDomain(hitDoc.get("domain"));
+				bean.setDesc(hitDoc.get("desc"));
+				bean.setSorttype(Integer.valueOf(hitDoc.get("sorttype")));
+				
+				alexList.add(bean);
+			}
+			isearcher.close();
+
+			return alexList;			
+		} catch (ParseException e) {
+			log.error("searchIndex ParseException:"+e.getMessage());
+			e.printStackTrace();
+
+			return null;
+		}
+		 catch (IOException e) {
+			log.error("searchIndex IOException:"+e.getMessage());
+			e.printStackTrace();
+
+			return null;
+		}
+		
+	}	
 }
