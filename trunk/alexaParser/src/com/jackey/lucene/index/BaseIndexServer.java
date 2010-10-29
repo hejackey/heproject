3a3765b94c57bac4f2a299e46548feb025dd2c4e
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2010-10-29 02:34:01 +0000

    最新版本

--- trunk/alexaParser/src/com/jackey/lucene/index/BaseIndexServer.java
+++ trunk/alexaParser/src/com/jackey/lucene/index/BaseIndexServer.java
@@ -3,12 +3,20 @@ package com.jackey.lucene.index;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Filter;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
+import org.apache.lucene.util.Version;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import com.jackey.lucene.util.ConfigUtil;
@@ -21,9 +29,26 @@ import com.jackey.lucene.util.ConfigUtil;
 public abstract class BaseIndexServer {
 	private Logger log = Logger.getLogger(BaseIndexServer.class);
 	
+	/**
+	 * 创建索引
+	 * @param writer
+	 */
+	public void createIndex(IndexWriter writer){
+		
+	}
 	
-	public void createIndex(IndexWriter writer){};
+	/**
+	 * 创建索引
+	 * @param writer
+	 */
+	public List searchIndex(String column,String key){
+		return null;
+	}	
 	
+	/**
+	 * 获取indexwriter索引器
+	 * @return
+	 */
 	public IndexWriter getIndexWriter(){
 		Analyzer analyzer = new IKAnalyzer();// 采用的分词器
 		
@@ -45,4 +70,34 @@ public abstract class BaseIndexServer {
 			return null;
 		}
 	}
+	
+	/**
+	 * 获取一个搜索器
+	 * @return
+	 */
+	public IndexSearcher getIndexSearch() {
+		
+		try {
+			Directory directory = FSDirectory.open(new File(ConfigUtil.getConfProperty(ConfigUtil.confFile, 
+					ConfigUtil.INDEX_FILE_PATH)));			
+
+			return new IndexSearcher(directory, true); 
+		} catch (IOException e) {
+			log.error("getIndexSearch IOException:"+e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * 获取一个queryparser
+	 * @param column 要查询的字段
+	 * @return
+	 */
+	public QueryParser getQueryParser(String column){
+		Analyzer analyzer = new IKAnalyzer();
+		
+		return new QueryParser(Version.LUCENE_30, column, analyzer);
+	}	
 }
--- trunk/alexaParser/src/com/jackey/lucene/test/TestAlexaIndexServer.java
+++ trunk/alexaParser/src/com/jackey/lucene/test/TestAlexaIndexServer.java
@@ -1,6 +1,9 @@
 package com.jackey.lucene.test;
 
+import java.io.IOException;
+
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.queryParser.ParseException;
 
 import com.jackey.lucene.index.AlexaIndexServer;
 import com.jackey.lucene.index.BaseIndexServer;
@@ -9,11 +12,15 @@ public class TestAlexaIndexServer {
 
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws ParseException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ParseException {
 		BaseIndexServer indexServer = new AlexaIndexServer();
 		IndexWriter writer = indexServer.getIndexWriter();
 		indexServer.createIndex(writer);
+		
+		indexServer.searchIndex("title", "»À»ÀÕ¯");		
 	}
 
 }
