09523718bb5cb3801306f2f6c900c442a1bdbc57
xiaohu0901 <xiaohu0901@hotmail.com>
2010-12-27 15:52:37 +0000

    toast和notification

--- AndroidDemo/src/com/he/android/demo/ToastActivity.java
+++ AndroidDemo/src/com/he/android/demo/ToastActivity.java
@@ -1,7 +1,12 @@
 package com.he.android.demo;
 
+
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +25,7 @@ public class ToastActivity extends Activity {
 			public void onClick(View v){
 				setTitle("段时间学习toast");
 				showToast(Toast.LENGTH_SHORT);
+				showNotification();
 			}
 		});
 		
@@ -47,5 +53,27 @@ public class ToastActivity extends Activity {
 	private View inflateView(int resource){
 		LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		return vi.inflate(resource, null);
+	}
+	
+	//自动发出notificaiton
+	protected void showNotification() {
+
+		NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		CharSequence title = "自动发出notificaiton";
+		CharSequence contents = "eoeandroid.com";
+
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, ActivityMain.class), 0);
+
+		Notification notification = new Notification(R.drawable.default_icon,
+				title, System.currentTimeMillis());
+
+		notification.setLatestEventInfo(this, title, contents, contentIntent);
+
+		// 100ms延迟后，振动250ms，停止100ms后振动500ms
+		notification.vibrate = new long[] { 100, 250, 100, 500 };
+
+		notificationManager.notify(R.layout.toast, notification);
 	}
 }
