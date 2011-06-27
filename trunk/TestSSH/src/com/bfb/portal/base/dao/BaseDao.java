2ae6ef9a71632846f77900c7a86df94a9e6120b5
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-06-27 08:43:25 +0000

    支持mybatis

--- trunk/TestSSH/src/com/bfb/portal/base/dao/BaseDao.java
+++ trunk/TestSSH/src/com/bfb/portal/base/dao/BaseDao.java
@@ -1,13 +1,12 @@
 package com.bfb.portal.base.dao;
 
-import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 public class BaseDao extends SqlSessionDaoSupport {
-	private SqlSession sqlSession;
+	/*private SqlSession sqlSession;
 
 	public void setSqlSession(SqlSession sqlSession) {
 		this.sqlSession = sqlSession;
-	}
+	}*/
 	
 }
