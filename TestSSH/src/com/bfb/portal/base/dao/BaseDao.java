251851f3f986033fe641de04e081d8f53085ade0
xiaohu0901 <xiaohu0901@hotmail.com>
2011-06-28 00:26:08 +0000

    删除extjs

--- TestSSH/src/com/bfb/portal/base/dao/BaseDao.java
+++ TestSSH/src/com/bfb/portal/base/dao/BaseDao.java
@@ -3,10 +3,5 @@ package com.bfb.portal.base.dao;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 public class BaseDao extends SqlSessionDaoSupport {
-	/*private SqlSession sqlSession;
-
-	public void setSqlSession(SqlSession sqlSession) {
-		this.sqlSession = sqlSession;
-	}*/
 	
 }
