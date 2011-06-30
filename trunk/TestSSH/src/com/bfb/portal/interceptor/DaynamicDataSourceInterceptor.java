956126d7a507488967ac556ed699c2b5db7857cc
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-06-30 09:15:09 +0000

    增加动态更新数据源

--- trunk/TestSSH/src/com/bfb/portal/interceptor/DaynamicDataSourceInterceptor.java
+++ trunk/TestSSH/src/com/bfb/portal/interceptor/DaynamicDataSourceInterceptor.java
@@ -2,11 +2,10 @@ package com.bfb.portal.interceptor;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import weblogic.jdbc.common.internal.RmiDataSource;
 
 import com.bfb.portal.base.util.EncypDataSource;
 import com.bfb.portal.base.util.StringUtil;
@@ -21,7 +20,6 @@ public class DaynamicDataSourceInterceptor implements Interceptor {
 	 */
 	private static final long serialVersionUID = 2355843001322874514L;
 	private static Logger log = Logger.getLogger(DaynamicDataSourceInterceptor.class);
-	private DriverManagerDataSource ds;
 	
 	public void destroy() {
 
@@ -31,10 +29,6 @@ public class DaynamicDataSourceInterceptor implements Interceptor {
 
 	}
 
-	/**
-	 * 动态加载数据源
-	 * 适用spring的DriverManagerDataSource和weblogic的jndi方式
-	 */
 	public String intercept(ActionInvocation invocation) throws Exception {
 		String actionName = ActionContext.getContext().getName();
 		log.info("actionName="+actionName);
@@ -48,40 +42,22 @@ public class DaynamicDataSourceInterceptor implements Interceptor {
 		}
 		
 		actionName = actionName.toLowerCase();
-		Object o = null;
+		DriverManagerDataSource ds = null;
 		if(actionName.startsWith("add") || actionName.startsWith("save")
 				|| actionName.startsWith("insert") || actionName.startsWith("create")
 				|| actionName.startsWith("update") || actionName.startsWith("edit")
 				|| actionName.startsWith("del") || actionName.startsWith("remove")
 				){
-			o = wac.getBean("master");
-			if( o instanceof RmiDataSource){
-				System.out.println(((RmiDataSource) o).getJNDINames()[0]);
-			}
-			else{
-				ds = (DriverManagerDataSource)o;
-			}
+			ds = (DriverManagerDataSource)wac.getBean("master");
 		}
 		else{
-			o = wac.getBean("slave");
-			if( o instanceof RmiDataSource){
-				System.out.println(((RmiDataSource) o).getJNDINames()[0]);
-			}
-			else{
-				ds = (DriverManagerDataSource)o;
-			}
+			ds = (DriverManagerDataSource)wac.getBean("slave");
 		}
 		
-		Object obj = wac.getBean("dataSource");
-		if( obj instanceof RmiDataSource){
-			obj = o;
-		}
-		else{
-			EncypDataSource dataSource = (EncypDataSource)obj;
-			dataSource.setUrl(ds.getUrl());
-			dataSource.setUsername(ds.getUsername());
-			dataSource.setPassword(ds.getPassword());
-		}
+		EncypDataSource dataSource = (EncypDataSource)wac.getBean("dataSource");
+		dataSource.setUrl(ds.getUrl());
+		dataSource.setUsername(ds.getUsername());
+		dataSource.setPassword(ds.getPassword());
 		
 		return invocation.invoke();
 	}
