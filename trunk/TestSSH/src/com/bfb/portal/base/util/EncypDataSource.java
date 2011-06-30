183d6272de04ab8d458b23b0b2647628998d96f8
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-06-30 01:11:16 +0000

    新增获取数据库链接配置文件中密码加密

---
+++ trunk/TestSSH/src/com/bfb/portal/base/util/EncypDataSource.java
@@ -0,0 +1,54 @@
+package com.bfb.portal.base.util;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * 继承spring的DriverManagerDataSource类，重新实现密码获取方式，配置文件中为加密后的密码
+ * @author Administrator
+ *
+ */
+public class EncypDataSource extends DriverManagerDataSource {
+	private String password;
+	private String driverClassName;
+	private String url;
+	private String username;
+	
+	public void setPassword(String password) {
+		try {
+			this.password = SecurityUtil.decrypt(password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Return the JDBC password to use for connecting through the Driver.
+	 */
+	public String getPassword() {
+		return this.password;
+	}
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
