3167d1c0458b23337566f33e8dd88fdaf800870d
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-06-30 09:25:42 +0000

    增加动态更新数据源

---
+++ trunk/TestSSH/src/com/bfb/portal/base/db/DataSourceSwitcher.java
@@ -0,0 +1,33 @@
+package com.bfb.portal.base.db;
+
+
+import org.springframework.util.Assert;
+
+public class DataSourceSwitcher {
+	@SuppressWarnings("unchecked")
+	private static final ThreadLocal contextHolder = new ThreadLocal();
+
+	@SuppressWarnings("unchecked")
+	public static void setDataSource(String dataSource) {
+		Assert.notNull(dataSource, "dataSource cannot be null");
+		contextHolder.set(dataSource);
+	}
+
+	public static void setMaster(){
+		clearDataSource();
+    }
+	
+	public static void setSlave() {
+		setDataSource("slave");
+	}
+	
+	public static String getDataSource() {
+		return (String) contextHolder.get();
+	}
+
+	public static void clearDataSource() {
+		contextHolder.remove();
+	}
+}
+
+
