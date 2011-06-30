5a4cf7eb750c675c43da5fe15a2220fe9f45e617
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-06-30 08:28:28 +0000

    增加动态更新数据源

--- trunk/TestSSH/src/com/bfb/portal/interceptor/DaynamicDataSourceInterceptor.java
+++ trunk/TestSSH/src/com/bfb/portal/interceptor/DaynamicDataSourceInterceptor.java
@@ -1,5 +1,14 @@
 package com.bfb.portal.interceptor;
 
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.bfb.portal.base.util.EncypDataSource;
+import com.bfb.portal.base.util.StringUtil;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
@@ -10,7 +19,8 @@ public class DaynamicDataSourceInterceptor implements Interceptor {
 	 * 
 	 */
 	private static final long serialVersionUID = 2355843001322874514L;
-
+	private static Logger log = Logger.getLogger(DaynamicDataSourceInterceptor.class);
+	
 	public void destroy() {
 
 	}
@@ -20,7 +30,38 @@ public class DaynamicDataSourceInterceptor implements Interceptor {
 	}
 
 	public String intercept(ActionInvocation invocation) throws Exception {
-		System.out.println(ActionContext.getContext().getName());
+		String actionName = ActionContext.getContext().getName();
+		log.info("actionName="+actionName);
+		
+		WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(
+				ServletActionContext.getServletContext());
+		
+		if(StringUtil.isEmpty(actionName)){
+			log.error("请求方法名为空");
+			return "";
+		}
+		
+		actionName = actionName.toLowerCase();
+		DriverManagerDataSource ds = null;
+		if(actionName.startsWith("add") || actionName.startsWith("save")
+				|| actionName.startsWith("insert") || actionName.startsWith("create")
+				|| actionName.startsWith("update") || actionName.startsWith("edit")
+				|| actionName.startsWith("del") || actionName.startsWith("remove")
+				){
+			ds = (DriverManagerDataSource)wac.getBean("master");
+		}
+		else{
+			ds = (DriverManagerDataSource)wac.getBean("slave");
+		}
+		
+		EncypDataSource dataSource = (EncypDataSource)wac.getBean("dataSource");
+		dataSource.setUrl(ds.getUrl());
+		dataSource.setUsername(ds.getUsername());
+		dataSource.setPassword(ds.getPassword());
+		
+		DataSourceTransactionManager manager = (DataSourceTransactionManager)wac.getBean("transactionManager");
+		manager.setDataSource(dataSource);
+		
 		
 		return invocation.invoke();
 	}
