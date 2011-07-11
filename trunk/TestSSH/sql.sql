5c7c974a7eb4f7f29d69481a4730badeef8d09ad
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-07-11 09:07:17 +0000

    建表sql

---
+++ trunk/TestSSH/sql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `contable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `h_id` int(8) DEFAULT NULL,
+  `con_value` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
