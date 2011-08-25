615312ebb3bf7658d80f162c62ca035d45a6c2af
xiaohu0901 <xiaohu0901@hotmail.com>
2011-08-25 06:25:06 +0000

    校验类变量是否安全：结论，struts中类变量多线程环境下是安全的，自己写的servlet中的类变量多线程环境下是不安全的

--- TestSSH/src/com/bfb/portal/servlet/TestServlet.java
+++ TestSSH/src/com/bfb/portal/servlet/TestServlet.java
@@ -12,9 +12,21 @@ public class TestServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = -4645562637814865539L;
-
+	private static int unsafeParam;//无论该变量是否定义成static，多线程环境下都是不安全的
+	private volatile int safeParam;
+	
 	public void doGet(HttpServletRequest req,HttpServletResponse res){
 		System.out.println("doGet ===============");
+		int safeP=0;//局部变量，多线程环境下是安全的
+		if("1".equals(req.getParameter("type"))){
+			testUnsafeParam(safeP);
+		}
+		unsafeParam = 1;
+		safeParam = 2;
+		safeP=3;
+		System.out.println("doGet ==============="+unsafeParam);
+		System.out.println("doGet =====safeParam=========="+safeParam);
+		System.out.println("doGet =====param=========="+safeP);
 		try {
 			res.getWriter().write("test struts servlet");
 		} catch (IOException e) {
@@ -24,5 +36,11 @@ public class TestServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req,HttpServletResponse res){
 		System.out.println("doPost ===============");
+	}
+	
+	public void testUnsafeParam(int param){
+		System.out.println("testUnsafeParam ==============="+unsafeParam);
+		System.out.println("testUnsafeParam =====safeParam=========="+safeParam);
+		System.out.println("testUnsafeParam =====param=========="+param);
 	}
 }
