11248075bc3e7f12e130e58002b0599c2d63eafa
xiaohu0901 <xiaohu0901@hotmail.com>
2011-08-29 07:05:07 +0000

    初始导入。

--- MavenDemo/src/main/java/com/bfb/commons/properties/PropertiesUtil.java
+++ MavenDemo/src/main/java/com/bfb/commons/properties/PropertiesUtil.java
@@ -1,5 +1,7 @@
 package com.bfb.commons.properties;
 
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
@@ -18,4 +20,29 @@ public class PropertiesUtil {
 		ResourceBundle rb = ResourceBundle.getBundle(proName);
 		return rb.getString(key);
 	}
+	
+	/**
+	 * 读取配置文件中指定key对应的值
+	 * @param fileName	读取的properties文件名	
+	 * @param keyName	取值的key
+	 * @return	返回key对应的value
+	 */
+	@SuppressWarnings("finally")
+	public static String getValueFromPropertyByKey(
+			String fileName,
+			String keyName) {
+			Properties initProps = new Properties();
+			String theValue = null;
+			InputStream in = null;
+
+			try {
+				in = PropertiesUtil.class.getResourceAsStream(fileName);
+				initProps.load(in);
+				theValue = initProps.getProperty(keyName);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				return theValue;
+			}
+		}
 }
