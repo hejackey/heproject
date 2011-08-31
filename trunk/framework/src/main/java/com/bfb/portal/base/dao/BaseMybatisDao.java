3cc3aadd1a20861a63aee3c9907b3d4f51cc2eda
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-08-31 09:20:23 +0000

    初始导入。

---
+++ trunk/framework/src/main/java/com/bfb/portal/base/dao/BaseMybatisDao.java
@@ -0,0 +1,12 @@
+package com.bfb.portal.base.dao;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+/**
+ * 基于mybatis3.0版本Dao
+ * @author Administrator
+ *
+ */
+public class BaseMybatisDao extends SqlSessionDaoSupport {
+	
+}
