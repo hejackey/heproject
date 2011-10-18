0e5d05cacc9c5353f238b26e93bc89d0f6ee91ff
xiaohu0901@hotmail.com <xiaohu0901@hotmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2011-10-18 09:08:05 +0000

    md5加密

---
+++ trunk/commonutil/src/main/java/com/bfb/commons/security/RsaUtil.java
@@ -0,0 +1,115 @@
+package com.bfb.commons.security;
+
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class RsaUtil {
+
+	/**
+	 * 生成密钥对
+	 * @return
+	 */
+	public static KeyPair genRsaKeyPair(){
+		try {
+			KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+			gen.initialize(1024);
+
+			return gen.generateKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			
+			return null;
+		}
+	}
+	
+	/**
+	 * rsa用公钥加密
+	 * @param pubKey 公钥
+	 * @param data	待加密数据
+	 * @return 加密数据
+	 */
+	public static String rsaEncry(PublicKey pubKey,String data){
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, pubKey);//指定加密模式
+			byte[] bytes = cipher.doFinal(data.getBytes());
+			
+			return SecurityUtil.byteArr2HexStr(bytes);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * rsa私钥解密
+	 * @param priKey	私钥
+	 * @param data	待解密数据
+	 * @return	解密后数据
+	 */
+	public static String rsaDecry(PrivateKey priKey,String data){
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE,priKey);
+			byte[] src = cipher.doFinal(SecurityUtil.hexStr2ByteArr(data));
+			
+			return new String(src);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		try {
+			
+			KeyPair pair = genRsaKeyPair();
+			PrivateKey priKey = pair.getPrivate();
+			PublicKey pubkey = pair.getPublic();
+			String pwd = rsaEncry(pubkey,"123adfbc324123");
+			
+			System.out.println(pwd);
+			
+			System.out.println(rsaDecry(priKey,pwd));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+}
