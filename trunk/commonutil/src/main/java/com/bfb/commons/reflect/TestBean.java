72931c2d3ad52e697f7499eca8109c59ff4bbfe3
he.jackey@gmail.com <he.jackey@gmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2012-01-09 09:18:21 +0000

    处理招行2次回调

---
+++ trunk/commonutil/src/main/java/com/bfb/commons/reflect/TestBean.java
@@ -0,0 +1,19 @@
+package com.bfb.commons.reflect;
+
+public class TestBean {
+	private String brandId;
+	private String cono;
+	
+	public String getBrandId() {
+		return brandId;
+	}
+	public void setBrandId(String brandId) {
+		this.brandId = brandId;
+	}
+	public String getCono() {
+		return cono;
+	}
+	public void setCono(String cono) {
+		this.cono = cono;
+	}
+}
