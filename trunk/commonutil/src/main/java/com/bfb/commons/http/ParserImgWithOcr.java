33c96cc5337a111542c11f42769267f40f9e16b2
he.jackey@gmail.com <he.jackey@gmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2012-01-12 05:46:09 +0000

    验证码图片识别

---
+++ trunk/commonutil/src/main/java/com/bfb/commons/http/ParserImgWithOcr.java
@@ -0,0 +1,42 @@
+package com.bfb.commons.http;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.asprise.util.ocr.OCR;
+
+/**
+ * 图片验证码识别程序，
+ * AspriseOCR.dll、DevIL.dll、ILU.dll拷贝到windows/system32目录下
+ * aspriseOCR.jar拷到classpath下，需要付费
+ * @author Administrator
+ * @version 1.0
+ * @date 2012-1-11
+ */
+public class ParserImgWithOcr {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String s = "";
+
+        try {
+               BufferedImage image = ImageIO.read(new File("d:\\8f8login.jpg"));
+               int width = image.getTileWidth();
+               int height = image.getTileHeight();
+               image = image.getSubimage(0, 0, width, height);
+               
+               s = new OCR().recognizeEverything(image);
+               System.out.println(s);
+        } catch (IOException e) {
+               e.printStackTrace();
+               System.out.println(" 图片识别失败！ ");
+        }
+
+	}
+
+}
