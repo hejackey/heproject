b580e0f87463c2d88d52a563b91dd04f3af1a102
he.jackey@gmail.com <he.jackey@gmail.com@9626de9c-e7c9-11dd-ab07-b19b4dad1b3d>
2012-02-08 05:27:49 +0000

    更新加密类

--- trunk/commonutil/src/main/java/com/bfb/commons/security/Md5Util.java
+++ trunk/commonutil/src/main/java/com/bfb/commons/security/Md5Util.java
@@ -1,11 +1,8 @@
 package com.bfb.commons.security;
 
-import java.security.MessageDigest;
 
-import org.apache.commons.lang.StringUtils;
 
 public class Md5Util {
-	private static final String DEFAULT_KEY="20zhuozhuo8f811"; 
 	/**
 	 * md5加密算法
 	 * 
@@ -16,30 +13,11 @@ public class Md5Util {
 	 */
 	public static String MD5(String value,String key) {
 		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			StringBuilder sb = new StringBuilder();
-			sb.append(value);
-			if(StringUtils.isEmpty(key)){
-				key = DEFAULT_KEY;
-			}
-			sb.append(key);
-			
-			md.update(sb.toString().getBytes("UTF8"));
-			byte s[] = md.digest();
-			
-			return SecurityUtil.byteArr2HexStr(s);
+			return SecurityUtil.encrypt(value, key);
 		} catch (Exception e) {
 			return null;
 		}
 
-	}
-
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		System.out.println(MD5("zhuozhuo","8f8apikey"));
 	}
 
 }
