d28d3bd48d5d884110e496622b0317692f9d4871
he.jackey <he.jackey@gmail.com>
2012-02-23 07:44:21 +0000

    DSA加密算法

--- commonutil/src/main/java/com/bfb/commons/security/RsaUtil.java
+++ commonutil/src/main/java/com/bfb/commons/security/RsaUtil.java
@@ -62,6 +62,36 @@ public class RsaUtil {
 	}
 	
 	/**
+	 * rsa用私钥加密
+	 * @param priKey 私钥
+	 * @param data	待加密数据
+	 * @return 加密数据
+	 */
+	public static String rsaEncry(PrivateKey priKey,String data){
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, priKey);//指定加密模式
+			byte[] bytes = cipher.doFinal(data.getBytes());
+			
+			return SecurityUtil.byteArr2HexStr(bytes);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
 	 * rsa私钥解密
 	 * @param priKey	私钥
 	 * @param data	待解密数据
@@ -71,6 +101,36 @@ public class RsaUtil {
 		try {
 			Cipher cipher = Cipher.getInstance("RSA");
 			cipher.init(Cipher.DECRYPT_MODE,priKey);
+			byte[] src = cipher.doFinal(SecurityUtil.hexStr2ByteArr(data));
+			
+			return new String(src);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * rsa公钥解密
+	 * @param pubKey	公钥
+	 * @param data	待解密数据
+	 * @return	解密后数据
+	 */
+	public static String rsaDecry(PublicKey pubKey,String data){
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE,pubKey);
 			byte[] src = cipher.doFinal(SecurityUtil.hexStr2ByteArr(data));
 			
 			return new String(src);
@@ -100,11 +160,11 @@ public class RsaUtil {
 			KeyPair pair = genRsaKeyPair();
 			PrivateKey priKey = pair.getPrivate();
 			PublicKey pubkey = pair.getPublic();
-			String pwd = rsaEncry(pubkey,"123adfbc324123");
+			String pwd = rsaEncry(priKey,"123adfbc324123");
 			
 			System.out.println(pwd);
 			
-			System.out.println(rsaDecry(priKey,pwd));
+			System.out.println(rsaDecry(pubkey,pwd));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
